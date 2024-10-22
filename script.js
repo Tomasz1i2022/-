c8e7a7f6776ddbdfcc56b4b7fadd5b7920aa14aa
Tomasz <1i2022tomasz14@gmail.com>
2024-10-22 08:50:29 +0200

    update js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let username = prompt("Podaj imię:");
 let number1 = parseInt(prompt("Podaj pierwszą liczbę: "));
 let number2 = parseInt(prompt("Podaj drugą liczbę: "));
-sum = number1 + number2;
-console.log(sum);
-document.write(sum);
+let operator = prompt("Wybierz operator działania: +,-,*,/,**")
+if (operator == +) {
+    result = number1 + number2
+} else if (operator == -) {
+    result = number1 - number2
+} else if (operator == *) {
+    result = number1 * number2
+} else if (operator == /) {
+    result = number1 / number2
+    if (number2 == 0){
+        document.write("Nie można dzielić przez 0!");
+    }
+} else if (operator == **) {
+    result = number1 ** number2
+} else {
+    document.write("Podano błędny operator!");
+};
+console.log(result);
+document.write(result);
