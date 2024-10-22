57359e53f91357b995a10f56ebb63fa3cb5c2a68
Tomasz <1i2022tomasz14@gmail.com>
2024-10-22 08:39:45 +0200

    created script

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+let username = prompt("Podaj imię:");
+let number1 = parseInt(prompt("Podaj pierwszą liczbę: "));
+let number2 = parseInt(prompt("Podaj drugą liczbę: "));
+sum = number1 + number2;
+console.log(sum);
+document.write(sum);
